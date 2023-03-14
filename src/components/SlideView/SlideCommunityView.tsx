8ac3c126310b53e9d13a7019cb5ca28d1f09cb5b
Hachiman12a1 <daovandat2k15@gmail.com>
2023-03-14 20:46:09 +0700

    Test PUSH

--- src/components/SlideView/SlideCommunityView.tsx
+++ src/components/SlideView/SlideCommunityView.tsx
@@ -1,69 +1,71 @@
 import {
-    ImageBackground,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-} from 'react-native'
-import React from 'react'
-import { LinearGradient } from 'expo-linear-gradient'
-import { BORDER, COLORS, SIZES } from '@theme'
+  ImageBackground,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { BORDER, COLORS, SIZES } from "@theme";
 
 interface slideProps {
-    title?: string
-    item: { id: string; title: string; image: string }
-    onPress?: () => void | undefined
+  title?: string;
+  item: { id: string; title: string; image: string };
+  onPress?: () => void | undefined;
 }
 
 const SlideCommunityView = ({ item, onPress }: slideProps) => {
-    return (
-        <TouchableOpacity style={styles.container} onPress={onPress}>
-            <View style={styles.container_Content}>
-                <Text style={styles.title}>{item.title}</Text>
-                <ImageBackground
-                    source={{ uri: item.image }}
-                    style={styles.image}
-                    imageStyle={{ borderRadius: BORDER.maximum }}
-                >
-                    <LinearGradient
-                        colors={['#140D2900', '#140D28E8']}
-                        style={styles.linearGradient}
-                    />
-                </ImageBackground>
-            </View>
-        </TouchableOpacity>
-    )
-}
+  console.log(1);
 
-export default SlideCommunityView
+  return (
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      <View style={styles.container_Content}>
+        <Text style={styles.title}>{item.title}</Text>
+        <ImageBackground
+          source={{ uri: item.image }}
+          style={styles.image}
+          imageStyle={{ borderRadius: BORDER.maximum }}
+        >
+          <LinearGradient
+            colors={["#140D2900", "#140D28E8"]}
+            style={styles.linearGradient}
+          />
+        </ImageBackground>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default SlideCommunityView;
 
 const styles = StyleSheet.create({
-    container: {
-        marginTop: 36,
-    },
+  container: {
+    marginTop: 36,
+  },
 
-    container_Content: {},
-    title: {
-        position: 'absolute',
-        bottom: 20,
-        left: 20,
-        color: COLORS.Neutral0,
-        zIndex: 99,
-        fontWeight: '600',
-        fontSize: SIZES.small,
-    },
-    image: {
-        position: 'relative',
-        zIndex: 1,
-        marginRight: 12,
-        width: 210,
-        height: 129,
-    },
-    linearGradient: {
-        width: '100%',
-        height: '100%',
-        borderRadius: BORDER.maximum,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
+  container_Content: {},
+  title: {
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    color: COLORS.Neutral0,
+    zIndex: 99,
+    fontWeight: "600",
+    fontSize: SIZES.small,
+  },
+  image: {
+    position: "relative",
+    zIndex: 1,
+    marginRight: 12,
+    width: 210,
+    height: 129,
+  },
+  linearGradient: {
+    width: "100%",
+    height: "100%",
+    borderRadius: BORDER.maximum,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
